ec02d13782737b8a1b34a3655726a4c9e136d139
trungj2655 <trungjoaoi2655@gmail.com>
2026-02-10 17:29:32 +0700

    0529

---
+++ Trung/DTH0/cau4.0.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double s, t1, t2;
+    cin >> s >> t1 >> t2;
+    cout << (s * 2) / (t1 + t2);
+}
---
+++ Trung/DTH0/cau4.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << (150 + 150) / (3 + 2);
+}
