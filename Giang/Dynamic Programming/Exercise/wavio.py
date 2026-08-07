f15d4dc6ee44f0a42cf1cda3179dcd00369f6506
Minh Giang <giangcopy24@gmail.com>
2026-08-07 11:35:54 +0700

    8/7/26

--- Giang/Dynamic Programming/Exercise/wavio.py
+++ Giang/Dynamic Programming/Exercise/wavio.py
@@ -1,15 +1,35 @@
-arr = [3, 1, 5, 2, 3, 6, 4, 5, 2, 1, 4]
+from bisect import bisect_left
+
+arr = [1, 2, 3, 4, 5, 4, 3]
 n = len(arr)
-dp = [1] * n
 
+inc = [0] * n
+tails = []
+
+for i, x in enumerate(arr):
+    pos = bisect_left(tails, x)
+    if pos == len(tails):
+        tails.append(x)
+    else:
+        tails[pos] = x
+    inc[i] = pos + 1
+
+dec = [0] * n
+tails = []
+
+for i in range(n - 1, -1, -1):
+    x = arr[i]
+    pos = bisect_left(tails, x)
+    if pos == len(tails):
+        tails.append(x)
+    else:
+        tails[pos] = x
+    dec[i] = pos + 1
+
+ans = 0
 for i in range(n):
-    for j in range(1, i):
-        if arr[j] > arr[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    ans = max(ans, 2 * min(inc[i], dec[i]) - 1)
 
-print(dp[n - 1])
-
-
-
-
- 
+print("inc:", inc)
+print("dec:", dec)
+print("wavio:", ans)
